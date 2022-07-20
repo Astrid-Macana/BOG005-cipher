33367d2889c9d479e7d303a280b0e14c5131d0cd
Luisa Saenz <lsaenzroa@gmail.com>
2022-07-19 22:53:09 -0500

    funcion cifrar en prueba

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,36 @@
 const cipher = {
-  
- 
-    
-  
-    
+  //window.addevelinstener("load",inicio,true),
 
+  //function inicio(){
+  //  document.getElementById("cifrar").addevelinstener("click",function(mensaje,clave))
+  //};
+
+  encode: function (offset, string) {
+    //codificar
+    let text = string;
+    var letra = 0;
+    var form = 0;
+    var txt2 = "";
+    for (let i = 0; i < text.length; i++) {
+      letra = Number(text.toUpperCase().charCodeAt(i));
+      form = ((letra - 65 + Number(offset)) % 26) + 65;
+      txt2 += String.fromCharCode(form);
+    }
+    return txt2;
+  },
+
+  decode: function (offset, string) {
+    //codificar
+    let text = string;
+    var letra = 0;
+    var form = 0;
+    var txt2 = "";
+    for (let i = 0; i < text.length; i++) {
+      letra = Number(text.toUpperCase().charCodeAt(i));
+      form = ((letra - 65 - Number(offset)) % 26) + 65;
+      txt2 += String.fromCharCode(form);
+    }
+    return txt2;
   }
+};
   export default cipher;
-
-
-    
--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import cipher from './cipher.js';
 
+document.getElementById("encode").addEventListener("click", cifrar);
 
-console.log(cipher);
+function cifrar() {
+  document.getElementById("mensaje2").value = cipher.encode(
+    document.getElementById("clave").value,
+    document.getElementById("mensaje").value
+  );
+}
 
+document.getElementById("decode").addEventListener("click", descifrar);
+
+function descifrar() {
+  document.getElementById("mensaje3").value = cipher.decode(
+    document.getElementById("clave").value,
+    document.getElementById("mensaje").value
+  );
+}
+
+let text = 'LA';
+var letra = 0;
+var form = 0;
+var txt2 = "";
+for (let i = 0; i < text.length; i++) {
+  letra = Number(text.toUpperCase().charCodeAt(i));
+  console.log("letra");
+  console.log(letra);
+  form = ((letra - 65 + 33) % 26) + 65;
+  console.log('FORM');
+  console.log(form);
+  txt2 += String.fromCharCode(form);
+  console.log("txt2");
+  console.log(txt2);
+}
+
+console.log('SIG')
