da63f57f72bcf72f7bf202d66e2432c93286c066
Luisa Saenz <lsaenzroa@gmail.com>
2022-07-20 23:03:29 -0500

    funciones cifrar y decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -20,17 +20,22 @@ const cipher = {
   },
 
   decode: function (offset, string) {
-    //codificar
+    //decodificar
     let text = string;
     var letra = 0;
     var form = 0;
     var txt2 = "";
     for (let i = 0; i < text.length; i++) {
       letra = Number(text.toUpperCase().charCodeAt(i));
-      form = ((letra - 65 - Number(offset)) % 26) + 65;
+      form = ((letra - 65 - Number(offset)) % 26);
+      if (form < 0){
+        form = form + 26 + 65; 
+      }else{
+        form = form + 65; 
+      }
       txt2 += String.fromCharCode(form);
     }
     return txt2;
   }
-};
+}
   export default cipher;
--- src/index.js
+++ src/index.js
@@ -17,21 +17,3 @@ function descifrar() {
     document.getElementById("mensaje").value
   );
 }
-
-let text = 'LA';
-var letra = 0;
-var form = 0;
-var txt2 = "";
-for (let i = 0; i < text.length; i++) {
-  letra = Number(text.toUpperCase().charCodeAt(i));
-  console.log("letra");
-  console.log(letra);
-  form = ((letra - 65 + 33) % 26) + 65;
-  console.log('FORM');
-  console.log(form);
-  txt2 += String.fromCharCode(form);
-  console.log("txt2");
-  console.log(txt2);
-}
-
-console.log('SIG')
